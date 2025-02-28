c304de9c8a075bcb3017b1339c56d6d087c3a9b6
abderramandidan <abderramandidan@localhost>
2025-02-27 17:07:09 -0700

    Created Playlist class.. will come back to tweak later (A.D)

---
+++ src/main/model/Playlist.java
@@ -0,0 +1,40 @@
+package main.model;
+
+import main.database.Song;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Playlist {
+    private String name; 
+    private List<Song> songs;
+
+    public Playlist(String name) {
+        this.name = name;
+        this.songs = new ArrayList<>();
+    }
+
+    public void addSong(Song song) {
+        if (!songs.contains(song)) {
+            songs.add(song);
+        }
+    }
+
+
+    public void removeSong(Song song) {
+        songs.remove(song);
+    }
+
+
+    public List<Song> getSongs() {
+        return new ArrayList<>(songs); 
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Playlist: %s (%d songs)", name, songs.size());
+    }
+}
